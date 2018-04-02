8cf8b87e034f0edafda12ea30f3ae2e4527603f2
Konstantin Lyakhov <kl@rifco.ru>
2018-04-02 14:24:07 +0300

    Update versions.py

--- workflow/env/Anaconda/versions.py
+++ workflow/env/Anaconda/versions.py
@@ -16,3 +16,16 @@ print('statsmodels: %s' % statsmodels.__version__)
 # scikit-learn
 import sklearn
 print('sklearn: %s' % sklearn.__version__)
+
+'''
+scipy: 1.0.0
+numpy: 1.14.0
+matplotlib: 2.1.2
+pandas: 0.22.0
+statsmodels: 0.8.0
+sklearn: 0.19.1
+
+tensorflow: 1.5.0
+Using TensorFlow backend.
+keras: 2.1.5
+'''
